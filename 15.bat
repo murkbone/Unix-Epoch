c6551c9f2c20d4082b7db45a66c6cf607be1fd9c
rocket <96433729+schneerocket@users.noreply.github.com>
2022-01-08 15:08:30 +0000

    Working non-approved custom cartridge for Unix Epoch
    Download this, then run it via Unix Epoch by typing "15".

---
+++ 15.bat
@@ -0,0 +1,10 @@
+@echo off
+title Testing, testing!
+cls
+
+:menu3245
+echo This works.
+echo I love you!
+set /p "sex=?: "
+if "%sex%" equ "bwefklufbhliugh2ql3ui4gb23piu9g2h3i4up2gb34poig42o3uigy4uog" exit
+call "%~dp0Cartridge 3.bat"
